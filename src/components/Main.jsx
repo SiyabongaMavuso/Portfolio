5ccff749fb87b0f19dc46ec9dc9152d1b34ca371
siyabongamavuso <siyabonga.auo@gmail.com>
2024-12-20 11:14:11 +0200

    fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
             <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
                 <h1 className='sm:text-4xl text-3xl font-bold text-gray-800'>Hi there, My name is Siyabonga Mavuso</h1>
-                <h2 className='flex sm:text-3xl text-2xl pt-4 yexy-gray-800'>He is a
+                <h2 className='flex sm:text-3xl text-2xl pt-4 yexy-gray-800'>I am a 
                 <TypeAnimation
                     sequence={[
                     'Developer', // Types 'Dveloper'
