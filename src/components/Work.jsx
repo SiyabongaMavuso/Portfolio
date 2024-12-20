acd46c2e24e78fcc3fe02a48d66aada490ef882a
siyabongamavuso <siyabonga.auo@gmail.com>
2024-12-20 12:28:49 +0200

    v1.1

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,32 +4,26 @@ import WorkItems  from './WorkItems'
 const data = [   
     {
         year: 2024,
+        title: 'Project underway',
+        duration: '1 month',
+        details: 'Build a relativley small nextjs & .net microservice wepapp api, Where each service will hanlde a dedictaed fuction in our application. Also have a serivce with rest based api crud operation. and a services to handle search functionality. Dfferent services use thier own Databses, Postgres and Mongo DB used in both for exceptional functionality. With the benefit of deploying our service independtly, Git/Github is our chosen and utilized version control with the peace of mind that we make changes , deploy independently of the other service.'
+
+    },
+    {
+        year: 2024,
         title: 'L1 Network Engineer (FSO) HCL Tech',
         duration: '7 months',
-        details: 'As an L1 Network Engineer, I implement, and maintain complex network infrastructure using RIP, OSPF, BGP, EIGRP, VLAN, STP, VPN, ACLs, and NAT. I configure switches, routers, firewalls, and load balancers, manage network security with TACACS, and DHCP Snooping, optimize performance with QoS and Cisco QOS, and monitor network health using Solarwinds, and Cisco Prime.'
+        details: 'As an L1 Network Engineer, I implement and maintain complex network infrastructure using RIP, OSPF, BGP, EIGRP, VLAN, STP, VPN, ACLs, and NAT. I configure switches, routers, firewalls, and load balancers, manage network security with TACACS, and DHCP Snooping, optimize performance with QoS and Cisco QOS, and monitor network health using Solarwinds, and Cisco Prime.'
 
     },
     {
         year: 2023,
-        title: 'Johannesburg Centre for Software Engineering (JCSE)',
-        duration: '3 months',
-        details: 'As a novice who has achieved Microsoft ERP MB920 certification with Wits University, demonstrating fundamental proficiency in utilizing Microsoft Dynamics 365 applications for effectively managing business operations and resources.'
+        title: 'Feild Support Enigneer (FSE) iOCO EOH',
+        duration: '7 months',
+        details: 'As a day-to-day IT support engineer at Sasol plant, I troubleshoot desktop related issues, maintain software systems crucial for production, and ensure seamless operations for clients. From resolving hardware malfunctions to optimizing software efficiency, I am the go-to expert for keeping technology aligned with the plants demanding requirements, ensuring uninterrupted workflows. Proactively monitoring incidents with ticket tracking system.'
 
     },
-    {
-        year: 2022,
-        title: 'CyberOps Essentials',
-        duration: '1 month',
-        details: 'Worked with configuring firewalls and IDSs, implemented security software, diagnosed, and troubleshoot network related problems. It is an entry-level position with technical knowledge and industry certifications.'
-
-    },
-    {
-        year: 2022,
-        title: 'School & Network Project',
-        duration: '3 years',
-        details: 'Uitenhage airport has been upgraded to allow for the use of full-sized aircrafts. Due to poor planning, they have not yet implemented the airport’s network infrastructure. ACSA (Uitenhage) has contacted our company to remedy the situation. So I and my team with network engineering backgrounds went to work in designing and implementing network infrastructure, creating LANs, WANS, VLANs, ACLS, NATs, VPNs, Switching, Routing and Wireless Essentials, Ethernet Switch Ports etc.',
-
-    }
+    
 
 ]
 const Work = () => {
